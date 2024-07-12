38e811a9bd375ab92d635bdb74d69c0b713c2a41
xxCary0113 <xx1076577400@gmail.com>
2024-07-12 17:08:50 +0800

    changed: python script

--- tools/atlas_sort.py
+++ tools/atlas_sort.py
@@ -8,7 +8,7 @@ def searchFiles(path, suffix):
     result = []
     files = os.listdir(path)
     for file in files:
-        filePath = str(path + "/" + file)
+        filePath = str(path + "\\" + file)
         if os.path.isfile(filePath):
             if filePath.endswith(suffix):
                 result.append(filePath)
@@ -20,7 +20,7 @@ def searchFiles(path, suffix):
 # 获取prefab中引用的图片
 def searchSpriteFromPrefab(prefabPath):
     result = []
-    prefabValue = json.load(open(prefabPath, "r"))
+    prefabValue = json.load(open(prefabPath, "r", encoding="utf-8"))
     stacks = [prefabValue]
     while len(stacks) > 0:
         item = stacks.pop(0)
@@ -35,44 +35,27 @@ def searchSpriteFromPrefab(prefabPath):
     return result
 
 
-# 根据路径获取module名称
-def analyseModuleName(path):
-    if "Module" in path:
-        return (path.split("Module")[0] + "Module" + "/".join(path.split("Module")[-1].split("/")[0:3])).replace("/_Atlas", "")
-    if "resources" in path:
-        return path.split("resources")[0] + "Module" + "/".join(path.split("resources")[-1].split("/")[0:3])
-    return None
-
-
-# 根据要移动的module和原始路径构造新路径
-def processPath(modulePath, filePath):
-    dstPath = modulePath + "/_Atlas"
-    currentPath = analyseModuleName(filePath)
-    paths = filePath.replace(currentPath, "").split("/")
-    if len(paths) > 1 and paths[-2] != "_Atlas" and paths[-2] != "Atlas" and paths[-2] != "Internal" and paths[-2] != "":
-        dstPath += "/" + paths[-2]
-    if len(paths) > 0:
-        dstPath += "/" + paths[-1]
-    return dstPath
 
 
 # 移动文件
 def moveFile(src, dst):
+    target_file = os.path.join(dst, os.path.basename(src))
+    if os.path.exists(target_file):
+        print("move dst exists")
+        return
     if not os.path.exists(src):
+        print("move src not exists")
         return
     if src == dst:
+        print("move src == dst")
         return
 
-    paths = dst.split("/")
-    currentPath = "."
-    while len(paths) > 1:
-        currentPath += "/" + paths.pop(0)
-        if not os.path.exists(currentPath):
-            os.mkdir(currentPath)
-    dst = "/".join(dst.split("/")[:-1])
-    shutil.move(src, dst)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        
+    shutil.move(src, dst, copy_function=shutil.copy2)
     if os.path.exists(src + ".meta"):
-        shutil.move(src + ".meta", dst)
+        shutil.move(src + ".meta", dst, copy_function=shutil.copy2)
 
 
 # 删除空文件夹
@@ -84,7 +67,7 @@ def deleteEmptyDir(path):
 
     isEmpty = True
     for file in files:
-        filePath = str(path + "/" + file)
+        filePath = str(path + "\\" + file)
         if os.path.isdir(filePath):
             nextIsEmpty = deleteEmptyDir(filePath)
             if not nextIsEmpty:
@@ -125,7 +108,7 @@ def addPacFile(path):
 
 
 def mainLogic():
-    projectPath = "../cocos_client/assets"
+    projectPath = "..\\cocos_client\\assets"
 
     # 收集所有图片信息
     imageMap = {}
@@ -137,7 +120,6 @@ def mainLogic():
         if os.path.exists(metaImage):
             imageJson = json.load(open(metaImage, "r"))
             uuid = ""
-            rawUuid = ""
             for key in imageJson["subMetas"]:
                 uuid = imageJson["subMetas"][key]["uuid"]
             if uuid != "":
@@ -146,75 +128,63 @@ def mainLogic():
     # 收集所有prefab引用的素材信息
     prefabs = searchFiles(projectPath, ".prefab") + searchFiles(projectPath, ".anim")
     refMap = {}
-    print("all prefabs:", prefabs)
-    return
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(current_dir)
+    assets_dir = os.path.join(root_dir, "cocos_client\\assets")
+    new_image_dir = os.path.join(assets_dir, "new_images")
+
     for prefab in prefabs:
         refSprites = searchSpriteFromPrefab(prefab)
-
-        moduleName = analyseModuleName(prefab)
-        if moduleName is None:
-            continue
+        ######## 处理目录
+        start = prefab.find('assets\\') + len('assets\\')  # start = 19
+        # 找到最后一个 '/' 的位置并计算 end 索引
+        end = prefab.rfind('.')  # end = 60
+        # 提取子字符串
+        extracted_path = prefab[start:end]
+        
         for refSprite in refSprites:
-            realUuid = refSprite
-            if realUuid in rawUuidMap:
-                realUuid = rawUuidMap[realUuid]
+            if refSprite not in refMap:
+                refMap[refSprite] = []
+                
+            exsit = False    
+            for path in refMap[refSprite]:
+                if path == extracted_path:
+                    exsit = True
+                    break
 
-            if realUuid not in refMap:
-                refMap[realUuid] = []
-            if moduleName not in refMap[realUuid]:
-                refMap[realUuid].append(moduleName)
-
-    # 根据图片出现次数调整
-    for uuid in imageMap:
-        # 判定是不是spine图片，spine不要处理
+            if not exsit:
+                refMap[refSprite].append(extracted_path)
+        
+                  
+    for uuid in imageMap: 
         imageURL = imageMap[uuid]
+        
+        if "abresources\\icon" in imageURL or "tiled_map\\t1" in imageURL:
+            continue
         if os.path.exists(imageURL.replace("." + imageURL.split(".")[-1], ".json")):
             continue
         if uuid not in refMap:
-            # 需要删除
-            os.remove(imageMap[uuid])
+            os.remove(imageURL)
         else:
             if len(refMap[uuid]) == 1:
                 # 只有1处引用
-                currentModule = analyseModuleName(refMap[uuid][0])
-                dstPath = processPath(currentModule, imageMap[uuid])
-                moveFile(imageMap[uuid], dstPath)
-            else:
-                isSameModule = True
-                isSameGroup = True
-                lastModuleName = ""
-                lastGroupName = ""
-                for item in refMap[uuid]:
-                    temps = item.split("/")
-                    moduleName = temps[-2]
-                    groupName = temps[-1]
-                    if lastModuleName != "" and moduleName != lastModuleName:
-                        isSameModule = False
-                        isSameGroup = False
-                        break
-                    if lastGroupName != "" and groupName != lastGroupName:
-                        isSameGroup = False
-                    lastModuleName = moduleName
-                    lastGroupName = groupName
-                if isSameModule:
-                    if isSameGroup:
-                        # 相同组，移动到同组的_Atlas下
-                        currentModule = analyseModuleName(refMap[uuid][0])
-                        dstPath = processPath(currentModule, imageMap[uuid])
-                        moveFile(imageMap[uuid], dstPath)
-                    else:
-                        # 不同组相同模块，移动到模块下的_Atlas
-                        dstPath = processPath("../client/assets/Module/" + lastModuleName, imageMap[uuid])
-                        moveFile(imageMap[uuid], dstPath)
-                else:
-                    dstPath = processPath("../client/assets/Module/_Global", imageMap[uuid])
-                    moveFile(imageMap[uuid], dstPath)
-
+                moveFile(imageURL, os.path.join(new_image_dir, refMap[uuid][0]))
+            if len(refMap[uuid]) > 1:
+                result_path = ""
+                for path in refMap[uuid]:
+                    result_path = result_path + "&" + path.split('\\')[-1]
+                    
+                if len(result_path) > 50:
+                    result_path = result_path[:50]
+            
+                moveFile(imageURL, os.path.join(new_image_dir, "common\\" + result_path))
+    
     # 清空空文件夹
     deleteEmptyDir(projectPath)
 
     # 添加图集文件
-    addPacFile(projectPath)
+    # addPacFile(projectPath)
 
 
 if __name__ == "__main__":
