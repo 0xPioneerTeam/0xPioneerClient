28b5fcba72b166e653049ae342ce4357660abad0
zhouchao7k <348894955@qq.com>
2024-07-12 17:08:29 +0800

    changed p_export tool

--- cocos_client/extensions/p_export/src/main.ts
+++ cocos_client/extensions/p_export/src/main.ts
@@ -3,20 +3,22 @@
  * @en Registration method for the main process of Extension
  */
 
-import {ExecuteSceneScriptMethodOptions } from "../@types/packages/scene/@types/public";
+import { ExecuteSceneScriptMethodOptions } from "../@types/packages/scene/@types/public";
 
 
 
 export const methods: { [key: string]: (...any: any) => any } = {
 
-    "menu_export":Func_Export
+    "menu_export": Func_Export
 };
 
 /**
  * @en Hooks triggered after extension loading is complete
  */
 export function load() {
-    console.warn("[ExportInfo]Export Function Ver=0.01");
+    Editor.Profile.getConfig("p_export").then(v => {
+        console.warn("[ExportInfo]Export:" + v.__version__);
+    })
 }
 
 /**
@@ -27,14 +29,13 @@ export function unload() { }
 
 //import packageJSON from '../package.json';
 //is method from cocos is error,so direct use package name here.
-const packageJSON_name:string  = "p_export";//
+const packageJSON_name: string = "p_export";//
 
-function Func_Export()
-{
+function Func_Export() {
     //this script can not access scene, must from execute-scene-script
     //console.log("button had press.");
 
-    const options:ExecuteSceneScriptMethodOptions = {
+    const options: ExecuteSceneScriptMethodOptions = {
         name: packageJSON_name,
         method: 'func_export',
         args: [10],
--- cocos_client/extensions/p_export/src/scene.ts
+++ cocos_client/extensions/p_export/src/scene.ts
@@ -1,13 +1,13 @@
 
 import { join } from 'path';
 import * as fs from "fs";
+import { INode } from '../@types/packages/scene/@types/public';
 
 //change path for import cc
 module.paths.push(join(Editor.App.path, 'node_modules'));
 
 
-import * as cc from "cc"
-
+// import * as cc from "cc"
 
 
 export function load() {
@@ -20,115 +20,128 @@ export function unload() {
 
 
 export const methods: { [key: string]: (...any: any) => any } = {
-
     "func_export": Func_Export
 };
 
 //find a node by uuid
-function GetNode(uuid: string, parent: cc.Node = null): cc.Node {
-    if (parent == null)
-        parent = cc.director.getScene();
-    var r = parent.getChildByUuid(uuid);
-    if (r != null)
-        return r;
-    for (var i = 0; i < parent.children.length; i++) {
-        var r = GetNode(uuid, parent.children[i]);
-        if (r != null)
-            return r;
-    }
+// function GetNode(uuid: string, parent: cc.Node = null): cc.Node {
+//     if (parent == null)
+//         parent = cc.director.getScene();
+//     var r = parent.getChildByUuid(uuid);
+//     if (r != null)
+//         return r;
+//     for (var i = 0; i < parent.children.length; i++) {
+//         var r = GetNode(uuid, parent.children[i]);
+//         if (r != null)
+//             return r;
+//     }
+//     return null;
+// }
 
-    return null;
-}
+var rootuuid = "";
+var subuuids = [];
 //get picked node
-function GetPickedNode(): cc.Node {
+async function GetPickedNode(): Promise<INode> {
     const type = Editor.Selection.getLastSelectedType();
     if (type != "node") {
         console.warn("pick type is node a node:" + type);
         return null;
-    }
-    else {
+    } else {
         var uuid = Editor.Selection.getLastSelected(type);
-        return GetNode(uuid, null);
+        let nodeData: INode = await Editor.Message.request('scene', 'query-node', uuid);
+        console.log("[ExportInfo]pick node=", String(nodeData.name.value));
+        rootuuid = String(nodeData.__prefab__.uuid);
+        return nodeData;
     }
-
 }
-class Pos2 {
+
+class Vec3 {
     x: number;
     y: number;
+    z: number;
 }
 
 class JsonItem {
-    id: string;
+    url: string;
     name: string;
     type: string;
     show: boolean;
     block: boolean;
     posmode: string;
-    positions: Pos2[];
-
+    positions: Vec3;
+    rotation: Vec3;
+    scale: Vec3;
+    children: JsonItem[];
 }
-function GetWorldPosArray(node: cc.Node): Pos2[] {
-    var pos = new Pos2();
-    pos.x = node.worldPosition.x;
-    pos.y = node.worldPosition.y;
-    var outpos: Pos2[] = [];
-    //test multi pos
-    node.children.forEach((child: cc.Node) => {
-        if (child.name.indexOf("Node") == 0) {
-            var cpos = new Pos2();
-            cpos.x = child.worldPosition.x;
-            cpos.y = child.worldPosition.y;
-            outpos.push(cpos);
 
-        }
-    });
-    if (outpos.length == 0) {
-        outpos.push(pos);
-    }
-    return outpos;
-}
+// function GetWorldPosArray(node: cc.Node): Pos2[] {
+//     var pos = new Pos2();
+//     pos.x = node.worldPosition.x;
+//     pos.y = node.worldPosition.y;
+//     var outpos: Pos2[] = [];
+//     //test multi pos
+//     node.children.forEach((child: cc.Node) => {
+//         if (child.name.indexOf("Node") == 0) {
+//             var cpos = new Pos2();
+//             cpos.x = child.worldPosition.x;
+//             cpos.y = child.worldPosition.y;
+//             outpos.push(cpos);
+
+//         }
+//     });
+//     if (outpos.length == 0) {
+//         outpos.push(pos);
+//     }
+//     return outpos;
+// }
 //Export,need comp MapTag,orelse block=false;
+async function tranINodeData(node: any) {
+    let iNode: INode;
+    if (node.uuid) {
+        iNode = node as INode;
+    } else if (node.value && node.value.uuid) {
+        iNode = await Editor.Message.request('scene', 'query-node', String(node.value.uuid));
+    } else {
+        return null;
+    }
+    var item = new JsonItem();
+    let pbuuid = iNode.__prefab__.uuid;
+    if(rootuuid == pbuuid){
+        item.url = '';
+    }else{
+        item.url = await Editor.Message.request('asset-db', 'query-url', pbuuid);
+    }
+    item.name = String(iNode.name.value);
+    item.show = Boolean(iNode.active.value);
+    item.positions = iNode.position.value as Vec3;
+    item.rotation = iNode.rotation.value as Vec3;
+    item.scale = iNode.scale.value as Vec3;
+    console.log(iNode);
+    item.children = [];
+    if(item.url == ''){// not other prefab check child  
+        for (let i = 0; i < iNode.children.length; i++) {
+            const child = iNode.children[i];
+            var subdata = await tranINodeData(child);
+            if(subdata){
+                item.children.push(subdata);
+            }
+        }
+    }
+    return item;
+}
+
 //need child with name "Node" for multi pos
-function Func_Export() {
-    var pick = GetPickedNode();
+async function Func_Export() {
+    var pick = await GetPickedNode();
     if (pick == null) {
         console.warn("[ExportInfo]Should pick export node first.");
         return;
     }
-
-
-    var outjson: JsonItem[] = [];
-    console.log("[ExportInfo]Pick=" + pick?.name + "  childcount=" + pick.children.length);
-    for (var i = 0; i < pick.children.length; i++) {
-
-        var node = pick.children[i];
-
-
-        var ctag = node.getComponent("MapTag");
-        var block: boolean = false;
-        if (ctag != null) {
-            block = (ctag as any).block;
-        }
-
-        //export to json
-        var item = new JsonItem();
-        item.id = "decorate_" + (i + 1).toString();
-        item.name = node.name;
-        item.type = "decorate";
-        item.show = true;
-        item.block = block;
-        item.posmode = "world";
-        item.positions = GetWorldPosArray(node);
-
-        outjson.push(item);
-    }
-
-
-    //save
+    var outjson = await tranINodeData(pick);
     {
         var outpath = join(Editor.Project.path, "outinfo.json");
         console.warn("[ExportInfo]output path = " + outpath);
-
+        console.warn("[ExportInfo]output path = " + outjson);
         fs.writeFileSync(outpath, JSON.stringify(outjson, null, 4));
     }
 }
