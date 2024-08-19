dda35db81a9c96803efaf5241e77dcf777eb9b76
zhouchao7k <348894955@qq.com>
2024-08-19 12:50:19 +0800

    Arms ui update

--- cocos_client/assets/Script/UI/Inner/ExerciseUI.ts
+++ cocos_client/assets/Script/UI/Inner/ExerciseUI.ts
@@ -11,11 +11,31 @@ export class ExerciseUI extends ViewController {
     private _title: Label = null;
     private _totalTroop: Label = null;
 
+    private _timeLabel: Label = null;
+    private _resourceItem:Node = null;
+    private _resourceContent:Node;
+    private _current_res_cur:Label;
+    private _current_res_max:Label;
+
+    private _list_Content:Node;
+    private _exerciseItem:Node;
 
     protected viewDidLoad(): void {
         super.viewDidLoad();
 
         this._title = this.node.getChildByPath("__ViewContent/_title").getComponent(Label);
+        const contentView = this.node.getChildByPath("__ViewContent");
+        this._resourceContent = contentView.getChildByPath("footer/material");
+        this._resourceItem = this._resourceContent.getChildByPath("Item");
+        this._resourceItem.removeFromParent();
+        this._timeLabel = contentView.getChildByPath("footer/txt_timeVal").getComponent(Label);
+
+        this._current_res_cur = contentView.getChildByPath("current_res/num/cur").getComponent(Label);
+        this._current_res_max = contentView.getChildByPath("current_res/num/max").getComponent(Label);
+
+        this._list_Content = contentView.getChildByPath("ScrollView/View/Content");
+        this._exerciseItem = this._list_Content.getChildByPath("ExercoseItem");
+        this._list_Content.removeAllChildren();
     }
 
     private async onTapClose() {
