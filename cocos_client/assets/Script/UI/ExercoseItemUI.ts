b37997b23b53ad01705ca88dc64ae9dac9863e7c
zhouchao7k <348894955@qq.com>
2024-08-19 12:50:19 +0800

    Arms ui update

---
+++ cocos_client/assets/Script/UI/ExercoseItemUI.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ExercoseItemUI')
+export class ExercoseItemUI extends Component {    
+
+    public async refreshUI() {
+      
+    }
+}
+
+
